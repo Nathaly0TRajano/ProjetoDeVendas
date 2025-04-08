efcd6140e3fa6022fe670d2ae0c3e8b91ff85d28
Nathaly0TRajano <nathydelima00@gmail.com>
2025-04-08 13:06:55 -0300

    IndexDoFuncionario

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Admin\Dashboard;
+use App\Livewire\Admin\Funcionario\FuncionarioIndex;
 use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,5 @@ Route::get('/', Login::class)->name('login');
 
 // Usuario tem que estar logado e ser um usuario do tipo admin
 Route::get('admin/dashboard', Dashboard::class)->name('admin.dashboard')->middleware(['auth','user_type:admin']);
+
+Route::get('admin/funcionario', FuncionarioIndex::class)->middleware(['auth', 'user_type:admin'])->name('admin.funcionario.index');
